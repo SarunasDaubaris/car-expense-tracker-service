c5b6395e2fa18b58ce2645a54236a667de349ae5
Sarunas <sarunas.daubaris@gmail.com>
2016-06-30 23:03:58 +0300

    Added REST integration base test

---
+++ src/test/java/com/personalgarage/service/integration/CarsIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.personalgarage.service.integration;
+
+import com.personalgarage.service.integration.base.BaseRestIntegrationTest;
+import org.junit.Test;
+
+public class CarsIntegrationTest extends BaseRestIntegrationTest {
+
+    @Test
+    public void test() {
+    }
+}
