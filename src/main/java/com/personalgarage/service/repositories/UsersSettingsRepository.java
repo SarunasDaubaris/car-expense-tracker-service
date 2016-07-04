135133c180dd529b5d011e369bc3c97171f64487
Sarunas <sarunas.daubaris@gmail.com>
2016-07-04 23:30:03 +0300

    More user repositories

--- src/main/java/com/personalgarage/service/repositories/UsersSettingsRepository.java
+++ src/main/java/com/personalgarage/service/repositories/UsersSettingsRepository.java
@@ -2,9 +2,7 @@ package com.personalgarage.service.repositories;
 
 import com.personalgarage.service.base.repositories.IRepository;
 import com.personalgarage.service.entities.UsersSettings;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsersSettingsRepository
         extends IRepository<UsersSettings, String> {
 }
---
+++ src/main/java/com/personalgarage/service/repositories/UsersSettingsTypesRepository.java
@@ -0,0 +1,8 @@
+package com.personalgarage.service.repositories;
+
+import com.personalgarage.service.base.repositories.IRepository;
+import com.personalgarage.service.entities.UsersSettingsTypes;
+
+public interface UsersSettingsTypesRepository
+        extends IRepository<UsersSettingsTypes, String> {
+}
