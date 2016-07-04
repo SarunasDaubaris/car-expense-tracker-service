7dd4a48771795339151e1170d51d46260f49b74e
Sarunas <sarunas.daubaris@gmail.com>
2016-07-04 23:46:37 +0300

    Users controller crud methods

--- src/main/java/com/personalgarage/service/controllers/UsersController.java
+++ src/main/java/com/personalgarage/service/controllers/UsersController.java
@@ -1,6 +1,46 @@
 package com.personalgarage.service.controllers;
 
 import com.personalgarage.service.base.controllers.BaseRestController;
+import com.personalgarage.service.dto.UsersDTO;
+import com.personalgarage.service.services.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 public class UsersController extends BaseRestController {
+
+    private UsersService usersService;
+
+    @Autowired
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET,
+            consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public UsersDTO get(@PathVariable("id") String id) {
+        return usersService.get(id);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST,
+            consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsersDTO insert(@RequestBody UsersDTO usersDTO) {
+        return usersService.insert(usersDTO);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.PUT,
+            consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public UsersDTO update(@RequestBody UsersDTO usersDTO) {
+        return usersService.update(usersDTO);
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE,
+            consumes = "application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") String id) {
+        usersService.delete(id);
+    }
 }
