83f82e250afe9ef8f51318bc17a4b65d9d60ee96
Šarūnas Daubaris <sarunas.daubaris@gmail.com>
2017-10-28 22:22:44 +0300

    Controller exception handler package rename

--- src/main/java/com/personalgarage/service/common/exceptionhandlers/ControllerExceptionHandler.java
+++ src/main/java/com/personalgarage/service/common/exceptionhandlers/ControllerExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.personalgarage.service.common.exception;
+package com.personalgarage.service.common.exceptionhandlers;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
