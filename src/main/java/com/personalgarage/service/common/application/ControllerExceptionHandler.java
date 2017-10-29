2b121e61e42766c6bc23dacc60ace4a9d99ee6d6
Šarūnas Daubaris <sarunas.daubaris@gmail.com>
2017-10-29 20:30:38 +0200

    Exception handler remove comment

--- src/main/java/com/personalgarage/service/common/application/ControllerExceptionHandler.java
+++ src/main/java/com/personalgarage/service/common/application/ControllerExceptionHandler.java
@@ -29,7 +29,6 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = {ApplicationServiceException.class})
     public ResponseEntity<Object> handleApplicationServiceException(RuntimeException ex, WebRequest request) {
         ApplicationServiceException applicationServiceException = (ApplicationServiceException) ex;
-//        String bodyOfResponse = applicationServiceException.getErrorData().getMessage();
         return handleExceptionInternal(ex, applicationServiceException.getErrorData(),
                 new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
