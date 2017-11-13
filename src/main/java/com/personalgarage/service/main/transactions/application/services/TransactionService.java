8eafbdcd25d230f839d8c9b6620e210b7640d5fa
Šarūnas Daubaris <sarunas.daubaris@gmail.com>
2017-11-13 21:21:25 +0200

    TransactionsService added Commons validator

--- src/main/java/com/personalgarage/service/main/transactions/application/services/TransactionService.java
+++ src/main/java/com/personalgarage/service/main/transactions/application/services/TransactionService.java
@@ -7,6 +7,7 @@ import com.personalgarage.service.main.transactions.interfaces.dtos.TransactionD
 import com.personalgarage.service.main.transactions.interfaces.errors.TransactionsErrors;
 import com.personalgarage.service.main.transactions.persistence.entities.Transaction;
 import com.personalgarage.service.main.transactions.persistence.repositories.TransactionRepository;
+import org.apache.commons.lang3.Validate;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,15 +33,16 @@ public class TransactionService implements ITransactionService {
 
     @Override
     public TransactionDTO createTransaction(TransactionDTO transactionDTO) {
-        if (transactionDTO.getCarId() == null) {
-            throw new ApplicationServiceException(TransactionsErrors.CAR_ID_INVALID);
-        }
-        if (transactionDTO.getUserId() == null) {
-            throw new ApplicationServiceException(TransactionsErrors.USER_ID_INVALID);
-        }
-        if (transactionDTO.getTransactionType() == null) {
-            throw new ApplicationServiceException(TransactionsErrors.TRANSACTION_TYPE_NULL);
-        }
+        Validate.notNull(transactionDTO, "transactionDTO is null");
+
+        Validate.notNull(transactionDTO.getCarId(), "carId is null");
+        Validate.isTrue(transactionDTO.getCarId() > 0, "carId is invalid");
+
+        Validate.notNull(transactionDTO.getUserId(), "userId is null");
+        Validate.isTrue(transactionDTO.getUserId() > 0, "userId is invalid");
+
+        Validate.notNull(transactionDTO.getTransactionType(), "transactionType is null");
+
         if (transactionDTO.getAmount() == null || BigDecimal.ZERO.compareTo(transactionDTO.getAmount()) >= 0) {
             throw new ApplicationServiceException(TransactionsErrors.AMOUNT_INVALID);
         }
@@ -52,11 +54,17 @@ public class TransactionService implements ITransactionService {
 
     @Override
     public TransactionDTO getTransactionById(Long id) {
+        Validate.notNull(id, "id is null");
+        Validate.isTrue(id > 0, "id is invalid");
+
         return transactionServiceModelMapper.mapByClass(transactionRepository.findOne(id), TransactionDTO.class);
     }
 
     @Override
     public List<TransactionDTO> getAllTransactionsByCarId(Long id) {
+        Validate.notNull(id, "id is null");
+        Validate.isTrue(id > 0, "id is invalid");
+
         return transactionServiceModelMapper.mapToListByClass(transactionRepository.findByCarId(id), TransactionDTO.class);
     }
 }
--- src/main/java/com/personalgarage/service/main/transactions/interfaces/errors/TransactionsErrors.java
+++ src/main/java/com/personalgarage/service/main/transactions/interfaces/errors/TransactionsErrors.java
@@ -5,10 +5,7 @@ import com.personalgarage.service.common.exceptions.ErrorData;
 
 public final class TransactionsErrors {
 
-    public static final ErrorData USER_ID_INVALID = new ErrorData(ErrorCodeBases.TRANSACTIONS, 1, "userId is invalid");
-    public static final ErrorData CAR_ID_INVALID = new ErrorData(ErrorCodeBases.TRANSACTIONS, 2, "carId is invalid");
-    public static final ErrorData TRANSACTION_TYPE_NULL = new ErrorData(ErrorCodeBases.TRANSACTIONS, 3, "transactionType is null");
-    public static final ErrorData AMOUNT_INVALID = new ErrorData(ErrorCodeBases.TRANSACTIONS, 4, "amount is invalid");
+    public static final ErrorData AMOUNT_INVALID = new ErrorData(ErrorCodeBases.TRANSACTIONS, 1, "amount is invalid");
 
     private TransactionsErrors() {
     }
